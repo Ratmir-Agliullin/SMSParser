0cd118b957ddd393b470763f5966fe4166dcb4e5
Ratmir-Agliullin <ratmir-91@mail.ru>
2017-10-07 12:45:02 +0400

    sms-activate work smsVk work

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -2,7 +2,12 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.ProtocolException;
+import java.net.URL;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -14,9 +19,51 @@ public class Parser {
 
     public static void main(String[] args) {
    //       SmsActivate();
-    // SimSMS();
-        SmsVK();
+ //  SimSMS();
+  //      SmsVK();
+     //   SmsLike();
+        try {
+            getPage();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
+
+
+
+    public static String SmsLike(){
+        String result=null;
+        link="https://smslike.ru/ru/";
+        Document doc = null;
+        try {
+            doc = Jsoup.connect(link).get();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        int i=0;
+        Element element = doc.select("div.pt-15").get(0);
+        while (true) {
+
+            try {
+               Element tr = element.getElementsByTag("tr").get(i);
+             String image = "https://smslike.ru"+tr.getElementsByTag("td").get(0).getElementsByTag("img").attr("src");
+                String name  = tr.getElementsByTag("td").get(0).getElementsByTag("span").get(0).text();
+                String count = tr.getElementsByTag("td").get(0).getElementsByTag("span").get(1).text();
+               String price = tr.getElementsByTag("td").get(0).getElementsByTag("div").get(0).text().split(" ")[1]+"р.";
+                i++;
+
+        System.out.println(image+" "+name+" "+count+" "+price);
+
+            } catch (IndexOutOfBoundsException e){
+                break;
+            }
+        }
+
+        return " ";
+    }
+
+
 
 
     public static String SmsVK(){
@@ -37,7 +84,8 @@ public class Parser {
               Element tr = element.getElementsByTag("tr").get(i);
               String name  = tr.getElementsByTag("td").get(0).text();
               String image = "http://smsvk.net"+tr.getElementsByTag("td").get(0).getElementsByTag("img").attr("src");
-        String count = tr.getElementsByTag("td").get(1).text();
+              String countBuf = tr.getElementsByTag("td").get(1).text();
+        String count = countBuf.substring(0,countBuf.length()-2);
         String price = tr.getElementsByTag("td").get(2).text();
                 i++;
         System.out.println(image+" "+name+" "+count+" "+price);
@@ -62,26 +110,16 @@ public class Parser {
             e.printStackTrace();
         }
         int i=0;
-      //  while (true) {
 
-          //  try {
-                Element element = doc.select("div.list-body").get(0);
-            Element li = element.getElementsByClass("li.row.list-item").get(0);
-            String image = "http://simsms.org"+li.getElementsByTag("img").get(0).attr("src");
-        //    Element div = element.getElementsByClass("col-xs-20").get(0);
-//                Element label = td.getElementsByTag("label").first();
-//                String name = label.getElementsByTag("span").first().text();
-//                String count = label.getElementsByTag("span").get(1).text();
-//                String price = element.getElementsByTag("td").get(2).text();
-//                i++;
-            //    System.out.println(name.substring(5)+" "+count.substring(0,count.length()-2)+" "+price);
-                System.out.println(image);
-         //   } catch (IndexOutOfBoundsException e){
-              //  break;
-          //  }
-     //   }
+//                Element element = doc.select("section.main-section").get(0);
+//            Element ul = element.getElementsByTag("ul").get(0);
+        Element ul = doc.select("section").get(0);
+//            String image = "http://simsms.org"+li.getElementsByTag("img").get(0).attr("src");
 
-        return " ";//name.substring(5)+" "+count.substring(0,count.length()-2)+" "+price;
+                System.out.println(ul.toString());
+
+
+        return " ";
     }
 
 
@@ -126,5 +164,25 @@ while (true) {
 
         }
         return result;
+    }
+
+    public static void getPage() throws IOException {
+        String url = "http://simsms.org/";
+
+        URL obj = new URL(url);
+        HttpURLConnection connection = (HttpURLConnection) obj.openConnection();
+
+        connection.setRequestMethod("GET");
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+
+        System.out.println(response.toString());
     }
 }
